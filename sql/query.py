11b82913c8a92ad074fac8742f5c10677f97de7a
ririgi <lrpbaltazar@gmail.com>
2019-08-02 09:09:33 +0800

    fix import statements

--- sql/query.py
+++ sql/query.py
@@ -6,7 +6,7 @@ sys.path.append('../')
 
 import os
 import time
-import pandas as pandas
+import pandas as pd
 import pymysql.cursors
 
 from utils import toCSV
@@ -27,12 +27,12 @@ if __name__ == '__main__':
 	query = '''SELECT USERID, SESSIONID, VIDEO_DURATION, USR_ACT_TOT_WATCHING_DUR FROM events_data
 			'''
 	outfile = "query_results/for_completion_total.csv"
-	main(query, outfile)
+	main(cursor, query, outfile)
 	query = '''SELECT EXTRACT(YEAR_MONTH FROM SESSION_STARTDT) AS MONTH, USERID, SESSIONID, VIDEO_DURATION, USR_ACT_TOT_WATCHING_DUR FROM events_data GROUP BY EXTRACT(YEAR_MONTH FROM SESSION_STARTDT)
 			'''
 	outfile = "query_results/for_completion_monthly.csv"
-	main(query, outfile)
+	main(cursor, query, outfile)
 	query = '''SELECT YEARWEEK(SESSION_STARTDT) AS WEEK, USERID, SESSIONID, VIDEO_DURATION, USR_ACT_TOT_WATCHING_DUR FROM events_data GROUP BY YEARWEEK(SESSION_STARTDT)
 			'''
 	outfile = "query_results/for_completion_weekly.csv"
-	main(query, outfile)
+	main(cursor, query, outfile)
