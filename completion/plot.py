975dbf2fe99aee0fb8e287799ee4e1e9174975f9
ririgi <lrpbaltazar@gmail.com>
2019-08-02 12:26:29 +0800

    add visualization of barplot

--- completion/plot.py
+++ completion/plot.py
@@ -10,17 +10,33 @@ import pandas as pd
 import numpy as np
 
 from utils import readChunk
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+import matplotlib.style as style
+
+sns.set()
+style.use('seaborn-poster')
+style.use('seaborn-talk')
+
+
 
 type_sess = ['total', 'less', '70']
-total = readChunk("../sql/query_result/month_count_total.csv")
 less70 = readChunk("../sql/query_result/month_count_less.csv")
 more70 = readChunk("../sql/query_result/month_count_70.csv")
 
-df = more70.merge(less70, on ='MONTH').merge(total, on = 'MONTH')
+df = more70.merge(less70, on ='MONTH')
 print(df.columns)
 
+df.rename(columns = {'NUMSESSIONS_X': 'SESSION_70', 'NUMSESSIONS_Y': 'SESSION_LESS'}, inplace = True)
 
-df['sum'] = df[col1] + df[col2]
-df['same'] = df['sum'] - df[col3]
-df['same'] = df['same'].apply(lambda x: 0 if x == 0 else 1)
-print('Total number of disimalr dates: ', df.same.sum())
+df.SESSION_70 = df.SESSION_70.astype(float)
+df.SESSION_LESS = df.SESSION_LESS.astype(float)
+df['sum'] = df.SESSION_70 + df.SESSION_LESS
+df.SESSION_70 = df[['SESSION_70', 'sum']].apply(lambda x: (x[0]/x[1])*100)
+df.SESSION_LESS = df[['SESSION_LESS', 'sum']].apply(lambda x: (x[0]/x[1])*100)
+
+
+
+df.set_index('MONTH').T.plot(kind = 'bar', stacked = True)
+plt.show()
