26d500eb44740705c47046b9ebad72891b6f2343
ririgi <lrpbaltazar@gmail.com>
2019-08-02 09:31:24 +0800

    add measuring of time in readChunk

--- utils.py
+++ utils.py
@@ -13,10 +13,14 @@ def readCSV(file, usecols = None, converters = None, encoding = None, dtype = st
 	return df
 
 def readChunk(file, usecols = None, converters = None, encoding = None, chunksize = 5000000, iterator = True, header = 'infer'):
+	s = time.time()
 	df = pd.read_csv(file, usecols = usecols, converters = converters, encoding = encoding,
 			chunksize = chunksize, iterator = iterator, header = header, low_memory = False, dtype = str)
 	df = pd.concat(df)
 	df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+	e = time.time()
+	total_time = time.strftime("%H:%M:%S", time.gmtime(e-s))
+	print("Finish reading file {} in {}".format(file, total_time))
 	return df
 
 def toCSV(df, file, index = True, encoding = None, sep = ','):
