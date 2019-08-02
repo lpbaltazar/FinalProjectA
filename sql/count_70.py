5acb7b69732e66b5f07240319a189fc3fcc9f67d
ririgi <lrpbaltazar@gmail.com>
2019-08-02 09:29:19 +0800

    edit counting and add extraction of customers

--- sql/count_70.py
+++ sql/count_70.py
@@ -15,14 +15,12 @@ file = "query_results/for_completion_total.csv"
 df = readChunk(file)
 
 def countCompletion70(df):
-	df.VIDEO_DURATION = df.VIDEO_DURATION.astype(float)
-	df.USR_ACT_TOT_WATCHING_DUR = df.USR_ACT_TOT_WATCHING_DUR.astype(float)
-	df["completion_70"] = (df.USR_ACT_TOT_WATCHING_DUR/df.VIDEO_DURATION)*100
-
+	df.COMPLETION_70 = df.COMPLETION_70.astype(float)
 	print("Total Number of Customers: {}".format(len(df.USERID.unique())))
 	df = df.loc[df.completion_70 >= 70]
 	print("Total Number of Customers with 70% Completion: {}".format(len(df.USERID.unique())))
 	print("\n")
+	return df
 
 def timeCompletion(df, col):
 	for time_comp in df[col].unique():
@@ -30,14 +28,26 @@ def timeCompletion(df, col):
 		temp = df.loc[df[col] == time_comp]
 		countCompletion70(temp)
 
+def getCustomers(df, col = False):
+	if col:
+		for time_comp in df[col].unique():
+			print(time_comp)
+			temp = df.loc[df[col] == time_comp]
+			temp = completion_70(temp)
+			temp = temp[['USERID', 'SESSIONID']]
+			toCSV(temp, 'customer70/'+col+'/'+time_comp+'.csv', index = False)
+
+	else:
+		temp = countCompletion70(df)
+		temp = temp[['USERID', 'SESSIONID']]
+		toCSV(temp, 'customer70/customer_completion_70.csv', index = False)
+
+
 if __name__ == '__main__':
 	file = "query_results/for_completion_total.csv"
 	df = readChunk(file)
+	df.rename(columns = {0:'USERID', 1:'SESSIONID', 2:'MONTH', 3:'WEEK', 4:'DATE', 5:'COMPLETION'}, inplace = True)
 	print("Entire time: ")
 	countCompletion70(df)
-	file = "query_results/for_completion_total.csv"
-	df = readChunk(file)
 	timeCompletion(df, 'MONTH')
-	file = "query_results/for_completion_total.csv"
-	df = readChunk(file)
 	timeCompletion(df, 'WEEK')
