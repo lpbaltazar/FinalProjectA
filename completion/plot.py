4908f19866b441fc761af736039384105dfaeece
ririgi <lrpbaltazar@gmail.com>
2019-08-02 11:39:24 +0800

    add checking of completion results

--- completion/plot.py
+++ completion/plot.py
@@ -18,3 +18,9 @@ more70 = readChunk("../sql/query_result/month_count_70.csv")
 
 df = more70.merge(less70, on ='MONTH').merge(total, on = 'MONTH')
 print(df.columns)
+
+
+df['sum'] = df[col1] + df[col2]
+df['same'] = df['sum'] - df[col3]
+df['same'] = df['same'].apply(lambda x: 0 if x == 0 else 1)
+print('Total number of disimalr dates: ', df.same.sum())
