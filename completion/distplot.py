dde11ff6513e331c522a18a0e6ca5515865202a0
ririgi <lrpbaltazar@gmail.com>
2019-08-05 01:41:28 +0800

    add custom binning

--- completion/distplot.py
+++ completion/distplot.py
@@ -28,4 +28,27 @@ df.COMPLETION = df.COMPLETION.astype(float)
 # plot = sns.distplot(df.COMPLETION.values, bins = 20, kde = False)
 plot = df.hist(column = 'COMPLETION', bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
 # plot.set(xlabel = 'Percent', ylabel = 'Number of Sessions')
-plt.show()
+plt.savefig('figures/trial1.png')
+
+bin1 = list(range(0, 105, 5))
+fordf = []
+print(len(bin1))
+for i in range(len(bin1)):
+	if i == 0: continue
+	elif i == 20: fordf.append("[{}, {}]".format(bin1[i-1], bin1[i]))
+	else: fordf.append("[{}, {})".format(bin1[i-1], bin1[i]))
+
+tohist = pd.DataFrame(index = fordf, columns = ['COUNT'])
+for i in range(len(bin1)):
+	if i == 0: continue
+	elif i == 20: temp = df.loc[(df.COMPLETION >= bin1[i-1]) & (df.COMPLETION <= bin1[i])]
+	else: temp = df.loc[(df.COMPLETION >= bin1[i-1]) & (df.COMPLETION < bin1[i])]
+
+	tohist.iloc[i-1]['COUNT'] = len(temp)
+
+
+plot = df.plot(kind = 'bar', colormap = 'Pastel2')
+plot.set_xlabel('COMPLETION RATE')
+plot.set_ylabel('Number of Sessions')
+plt.tight_layout()
+plt.savefig('figures/trial2.png')
