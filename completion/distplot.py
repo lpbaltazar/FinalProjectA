89950d1542d60f279b92d60e1e17cc611e82e051
ririgi <lrpbaltazar@gmail.com>
2019-08-05 01:13:06 +0800

    add code for distrib plot

--- completion/distplot.py
+++ completion/distplot.py
@@ -20,7 +20,12 @@ style.use('seaborn-poster')
 
 df = readChunk("../sql/query_results/all_session_completion.csv")
 df.dropna(subset = ['COMPLETION'], inplace = True)
+print(df.head())
 df.COMPLETION = df.COMPLETION.astype(float)
 
-plot = sns.distplot(df.COMPLETION.values)
+# df = df.loc[df.COMPLETION >= 70.0]
+# print(df.head())
+# plot = sns.distplot(df.COMPLETION.values, bins = 20, kde = False)
+plot = df.hist(column = 'COMPLETION', bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+# plot.set(xlabel = 'Percent', ylabel = 'Number of Sessions')
 plt.show()
