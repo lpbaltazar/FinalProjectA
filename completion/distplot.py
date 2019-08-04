4c981ea0aa5679300042a8c736f00d253360f25f
ririgi <lrpbaltazar@gmail.com>
2019-08-05 01:45:28 +0800

    add custom binning

--- completion/distplot.py
+++ completion/distplot.py
@@ -26,7 +26,7 @@ df.COMPLETION = df.COMPLETION.astype(float)
 # df = df.loc[df.COMPLETION >= 70.0]
 # print(df.head())
 # plot = sns.distplot(df.COMPLETION.values, bins = 20, kde = False)
-plot = df.hist(column = 'COMPLETION', bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+plot = df.hist(column = 'COMPLETION', bins = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])
 # plot.set(xlabel = 'Percent', ylabel = 'Number of Sessions')
 plt.savefig('figures/trial1.png')
 
