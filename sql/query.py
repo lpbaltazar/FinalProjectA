6d90e8ab96e76a488fce24d96b6338e68a5c94f4
ririgi <lrpbaltazar@gmail.com>
2019-08-05 10:52:54 +0800

    fix query

--- sql/query.py
+++ sql/query.py
@@ -24,35 +24,35 @@ def main(cursor, query, outfile):
 
 if __name__ == '__main__':
 	cursor = pymysql.connect(host = "localhost", user = 'rigi', password = 'pwd@rigi', db = 'events_db')
-	query = '''SELECT YEARWEEK(SESSION_STARTDT, 1) AS WEEK, USERID, COUNT(SESSIONID)
-				FROM events_data
-				WHERE (SUM(USR_ACT_TOT_WATCHING_DUR)*1.0)/(SUM(VIDEO_DURATION)*1.0)) >= 0.70
-				GROUP BY YEARWEEK(SESSION_STARTDT, 1), USERID)
-				'''
-	outfile = 'query_results/customer_sessioncount_week.csv'
-	main(cursor, query, outfile)
+	# query = '''SELECT YEARWEEK(SESSION_STARTDT, 1) AS WEEK, USERID, COUNT(SESSIONID)
+	# 			FROM events_data
+	# 			GROUP BY YEARWEEK(SESSION_STARTDT, 1), USERID
+	# 			HAVING (SUM(USR_ACT_TOT_WATCHING_DUR)/SUM(VIDEO_DURATION))*100 >= 70
+	# 			'''
+	# outfile = 'query_results/customer_sessioncount_week.csv'
+	# main(cursor, query, outfile)
 
-	query = '''SELECT EXTRACT(YEAR_MONTH FROM SESSION_STARTDT) AS MONTH, USERID, COUNT(SESSIONID)
+	query = '''SELECT EXTRACT(YEAR_MONTH FROM SESSION_STARTDT) AS MONTH, USERID, COUNT(SESSIONID) AS FREQUENCY
 				FROM events_data
-				WHERE (SUM(USR_ACT_TOT_WATCHING_DUR)*1.0)/(SUM(VIDEO_DURATION)*1.0)) >= 0.70
 				GROUP BY EXTRACT(YEAR_MONTH FROM SESSION_STARTDT), USERID
+				HAVING ((SUM(USR_ACT_TOT_WATCHING_DUR)*1.0)/(SUM(VIDEO_DURATION)*1.0)) >= 0.70
 				'''
 	outfile = 'query_results/customer_sessioncount_month.csv'
 	main(cursor, query, outfile)
 
-	query = '''SELECT USERID, COUNT(SESSIONID)
+	query = '''SELECT USERID, COUNT(SESSIONID) AS FREQUENCY
 				FROM events_data
-				WHERE (SUM(USR_ACT_TOT_WATCHING_DUR)*1.0)/(SUM(VIDEO_DURATION)*1.0)) >= 0.70
 				GROUP BY USERID
+				HAVING ((SUM(USR_ACT_TOT_WATCHING_DUR)*1.0)/(SUM(VIDEO_DURATION)*1.0)) >= 0.70
 				'''
 	outfile = 'query_results/customer_sessioncount_all.csv'
 	main(cursor, query, outfile)
 
 
-	query = '''SELECT DATE(SESSION_STARTDT) AS DATE, USERID, COUNT(SESSIONID)
+	query = '''SELECT DATE(SESSION_STARTDT) AS DATE, USERID, COUNT(SESSIONID) AS FREQUENCY
 				FROM events_data
-				WHERE (SUM(USR_ACT_TOT_WATCHING_DUR)*1.0)/(SUM(VIDEO_DURATION)*1.0)) >= 0.70
 				GROUP BY DATE(SESSION_STARTDT), USERID
+				HAVING ((SUM(USR_ACT_TOT_WATCHING_DUR)*1.0)/(SUM(VIDEO_DURATION)*1.0)) >= 0.70
 				'''
 	outfile = 'query_results/customer_sessioncount_day.csv'
 	main(cursor, query, outfile)
