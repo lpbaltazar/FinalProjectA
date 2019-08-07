550b76880ad331494872288447ada20880cd5981
ririgi <lrpbaltazar@gmail.com>
2019-08-07 10:27:41 +0800

    fix bugs

--- frequency/returning.py
+++ frequency/returning.py
@@ -13,20 +13,19 @@ from utils import readChunk, toCSV
 
 completion = 80
 
-file = 'customer_sessioncount_'+str(completion)+'_month.csv'
+file = '../sql/query_results/customer_sessioncount_'+str(completion)+'_month.csv'
 out = 'results/'+str(completion)+'_MONTH_RETURN_VALUE.csv'
 col = 'MONTH'
 colname = col+'_RETURN_VALUE'
 
 
+s = time.time()
 df = readChunk(file)
 df.rename(columns = {'COUNT(SESSIONID)': 'FREQUENCY'}, inplace = True)
 df.FREQUENCY = df.FREQUENCY.astype(int)
 
 new_df = pd.DataFrame(index = df.USERID.unique(), columns = [colname], data = 0)
 new_df.index.name = 'USERID'
-new_df.reset_index(inplace = True)
-print(new_df.columns)
 
 if col == 'MONTH':
 	init = list(df.loc[df.MONTH == '201812'].USERID.unique())
@@ -35,12 +34,13 @@ elif col == 'WEEK':
 
 def addvalue(x):
 	x = x+1
+	print('here')
 	return x
 
-
+df[col] = df[col].astype(float)
 for i in df[col].unique():
-	df[col] = df[col].astype(float)
-	if col == 'MONTH]':
+	print(i)
+	if col == 'MONTH':
 		if i < 201812: continue
 	elif col == 'WEEK':
 		if i < 201848: continue
@@ -48,11 +48,19 @@ for i in df[col].unique():
 	temp = df.loc[df[col] == i]
 	users = list(temp.USERID.unique())
 	common = list(set(init).intersection(users))
-	new_df[colname] = new_df.apply(lambda x: addvalue(x.MONTH_RETURN_VALUE) if x.USERID in common else x[colname], axis = 1)
+	# for j in range(len(common)):
+	# 	new_df.loc[common[j]][colname] = new_df.loc[common[j]][colname] + 1
+
+	new_df[colname] = new_df.apply(lambda x: addvalue(x[colname]) if str(x.index) in common else x[colname], axis = 1)
 	init.extend(users)
 	init = list(set(init))
 
 print(new_df.head())
+print(new_df[colname].unique())
 toCSV(new_df, out, index = False)
+
+e = time.time()
+total_time = time.strftime("%H:%M:%S", time.gmtime(e-s))
+print("Total time: ", total_time)
 
 
