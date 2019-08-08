5fc061bb3cdb71919d0fdd4a83177758daf89984
ririgi <lrpbaltazar@gmail.com>
2019-08-08 14:34:43 +0800

    add code for getting plateau of frequency and completion

---
+++ frequency/plateau.py
@@ -0,0 +1,25 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import sys
+sys.path.append("../")
+
+import os
+import time
+import pandas as pd
+import numpy as np
+
+from utils import readChunk
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+import matplotlib.style as style
+
+sns.set()
+style.use('seaborn-poster')
+
+df = readChunk('../sql/query_results/plateu_all.csv')
+df.rename(columns = {'COUNT(SESSIONID)':'FREQUENCY'}, inplace = True)
+
+plot = sns.regplot(x = 'FREQUENCY', y = 'COMPLETION', data = df, scatter = True, fit_reg = False)
+plt.show()
