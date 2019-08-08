9b635ba55ec8f9d3e785e9b660f0a0f4a1eb9436
ririgi <lrpbaltazar@gmail.com>
2019-08-08 14:36:05 +0800

    add conversion of data type

--- frequency/plateau.py
+++ frequency/plateau.py
@@ -21,5 +21,7 @@ style.use('seaborn-poster')
 df = readChunk('../sql/query_results/plateu_all.csv')
 df.rename(columns = {'COUNT(SESSIONID)':'FREQUENCY'}, inplace = True)
 
+df.FREQUENCY = df.FREQUENCY.astype(int)
+df.COMPLETION = df.COMPLETION.astype(float)
 plot = sns.regplot(x = 'FREQUENCY', y = 'COMPLETION', data = df, scatter = True, fit_reg = False)
 plt.show()
