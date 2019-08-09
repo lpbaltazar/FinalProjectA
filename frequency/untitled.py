ae8dfc81435aff17b2a28ca19d1ddebcf405cd6c
ririgi <lrpbaltazar@gmail.com>
2019-08-09 09:07:59 +0800

    edit colname for total engagement

--- frequency/untitled.py
+++ frequency/untitled.py
@@ -8,11 +8,11 @@ from utils import readChunk, toCSV
 
 df = readChunk("../../events/MONTH_SESSION_TIME_CATEGORY_DURATION.csv")
 
-df.rename(columns = {0:'MONTH', 1:'USERID', 2:'SESSIONID', 3:'STARTHOUR', 4:'ENDHOUR', 5:'SESSIONDURATION'}, inplace = True)
+df.rename(columns = {0:'MONTH', 1:'USERID', 2:'SESSIONID', 3:'STARTHOUR', 4:'ENDHOUR', 5:'engagement'}, inplace = True)
 df.SESSIONDURATION = df.SESSIONDURATION.astype(float)
 df.MONTH = df.MONTH.astype(int)
 df = df.loc[df.MONTH >= 201812]
 
-total_df = df.groupby('USERID')['SESSIONDURATION'].sum().to_frame()
+total_df = df.groupby('USERID')['engagement'].sum().to_frame()
 print(total_df.head())
 toCSV(total_df, 'results/overall_engagement.csv')
