905868121c8f1c40411d8909e384ebd3f34925c8
ririgi <lrpbaltazar@gmail.com>
2019-08-11 21:04:38 +0800

    edit click distribution plot by adding plotting of each cluster

--- characterization/click.py
+++ characterization/click.py
@@ -19,20 +19,28 @@ sns.set()
 style.use('seaborn-poster')
 style.use('bmh')
 
-df = readChunk('CLICK.csv')
+clusters = readChunk('rfe_clustering_5.csv')
+clusters.columns = clusters.columns.str.upper()
+print(clusters.head())
+
 cols = ['ADPLAY_COUNT', 'PLAY_COUNT', 'PAUSE_COUNT', 'RESUME_COUNT']
 for i in cols:
 	df[i] = df[i].astype(int)
 
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
-sns.distplot(df.ADPLAY_COUNT.values, color = 'steelblue', ax = ax1)
-ax1.set_title('ADPLAY')
-sns.distplot(df.PLAY_COUNT.values, color = 'steelblue', ax = ax2)
-ax2.set_title('PLAY')
-sns.distplot(df.PAUSE_COUNT.values, color = 'steelblue', ax = ax3)
-ax3.set_title('PAUSE')
-sns.distplot(df.RESUME_COUNT.values, color = 'steelblue', ax = ax4)
-ax4.set_title('RESUME')
+clusters.LABEL = clusters.LABEL.astype(int)
+clusters = clusters.merge(df, how = 'left', on = 'USERID')
 
-plt.tight_layout()
-plt.show()
+for i in clusters.LABEL.unique():
+	temp = clusters.loc[clusters.LABEL == i]
+	fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+	sns.distplot(temp.ADPLAY_COUNT.values, color = 'steelblue', ax = ax1)
+	ax1.set_title('ADPLAY')
+	sns.distplot(temp.PLAY_COUNT.values, color = 'steelblue', ax = ax2)
+	ax2.set_title('PLAY')
+	sns.distplot(temp.PAUSE_COUNT.values, color = 'steelblue', ax = ax3)
+	ax3.set_title('PAUSE')
+	sns.distplot(temp.RESUME_COUNT.values, color = 'steelblue', ax = ax4)
+	ax4.set_title('RESUME')
+
+	plt.tight_layout()
+	plt.show()
