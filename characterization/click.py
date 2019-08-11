1a3f0a06e21f2cc014ce9525b3fc0015a3703788
ririgi <lrpbaltazar@gmail.com>
2019-08-11 21:07:06 +0800

    edit bugs

--- characterization/click.py
+++ characterization/click.py
@@ -19,10 +19,7 @@ sns.set()
 style.use('seaborn-poster')
 style.use('bmh')
 
-clusters = readChunk('rfe_clustering_5.csv')
-clusters.columns = clusters.columns.str.upper()
-print(clusters.head())
-
+df = readChunk('CLICK.csv')
 cols = ['ADPLAY_COUNT', 'PLAY_COUNT', 'PAUSE_COUNT', 'RESUME_COUNT']
 for i in cols:
 	df[i] = df[i].astype(int)
@@ -32,6 +29,7 @@ clusters = clusters.merge(df, how = 'left', on = 'USERID')
 
 for i in clusters.LABEL.unique():
 	temp = clusters.loc[clusters.LABEL == i]
+
 	fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
 	sns.distplot(temp.ADPLAY_COUNT.values, color = 'steelblue', ax = ax1)
 	ax1.set_title('ADPLAY')
@@ -44,3 +42,4 @@ for i in clusters.LABEL.unique():
 
 	plt.tight_layout()
 	plt.show()
+	plt.clf()
