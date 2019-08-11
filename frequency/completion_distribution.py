cb487596b5a1e897291543b1bf7b6fb8b4770b78
ririgi <lrpbaltazar@gmail.com>
2019-08-12 02:12:11 +0800

    make plots for completion distribution subplots

--- frequency/completion_distribution.py
+++ frequency/completion_distribution.py
@@ -25,10 +25,8 @@ df.FREQUENCY = df.FREQUENCY.astype(int)
 df.COMPLETION = df.COMPLETION.astype(float)
 df.dropna(subset = ['COMPLETION'], inplace = True)
 
-temp = df.loc[df.FREQUENCY >= 51]
-# temp = df.loc[df.FREQUENCY <= ]
-print(len(temp))
-print(temp.head())
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1, 5)
+temp = df.loc[df.FREQUENCY == 1]
 bin1 = list(range(0, 110, 10))
 fordf = []
 print(len(bin1))
@@ -47,13 +45,125 @@ for i in range(len(bin1)):
 	tohist.iloc[i-1]['COUNT'] = len(temp1)
 
 tot = tohist.COUNT.sum()
-plot = tohist.plot(kind = 'bar', colormap = 'Pastel2')
+tohist.plot(kind = 'bar', color = 'steelblue', ax = ax1, legend = False)
 tohist['percent'] = (tohist['COUNT']/tot)*100
 tohist['percent'] = tohist['percent'].astype(float)
 tohist['percent'] = round(tohist['percent'], 1)
-# tohist['percent'] = round(tohist['percent'], 1)
 for i in range(tohist.shape[0]):
 	plot.text(i, tohist.iloc[i]['COUNT'], str(tohist.iloc[i]['percent']), horizontalalignment = 'center')
+
+ax1.set_title('ONE TIME USERS')
+
+temp = df.loc[(df.FREQUENCY >= 2) & (df.FREQUENCY <= 8)]
+bin1 = list(range(0, 110, 10))
+fordf = []
+print(len(bin1))
+for i in range(len(bin1)):
+	if i == 0: continue
+	elif i == len(bin1): fordf.append("[{}, {}]".format(bin1[i-1], bin1[i]))
+	else: fordf.append("[{}, {})".format(bin1[i-1], bin1[i]))
+
+tohist = pd.DataFrame(index = fordf, columns = ['COUNT'])
+for i in range(len(bin1)):
+	print(bin1[i])
+	if i == 0: continue
+	elif i == len(bin1): temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION <= bin1[i])]
+	else: temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION < bin1[i])]
+
+	tohist.iloc[i-1]['COUNT'] = len(temp1)
+
+tot = tohist.COUNT.sum()
+tohist.plot(kind = 'bar', color = 'steelblue', ax = ax2, legend = False)
+tohist['percent'] = (tohist['COUNT']/tot)*100
+tohist['percent'] = tohist['percent'].astype(float)
+tohist['percent'] = round(tohist['percent'], 1)
+for i in range(tohist.shape[0]):
+	plot.text(i, tohist.iloc[i]['COUNT'], str(tohist.iloc[i]['percent']), horizontalalignment = 'center')
+
+ax2.set_title('SELDOM USERS')
+
+temp = df.loc[(df.FREQUENCY >= 9) & (df.FREQUENCY <= 14)]
+bin1 = list(range(0, 110, 10))
+fordf = []
+print(len(bin1))
+for i in range(len(bin1)):
+	if i == 0: continue
+	elif i == len(bin1): fordf.append("[{}, {}]".format(bin1[i-1], bin1[i]))
+	else: fordf.append("[{}, {})".format(bin1[i-1], bin1[i]))
+
+tohist = pd.DataFrame(index = fordf, columns = ['COUNT'])
+for i in range(len(bin1)):
+	print(bin1[i])
+	if i == 0: continue
+	elif i == len(bin1): temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION <= bin1[i])]
+	else: temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION < bin1[i])]
+
+	tohist.iloc[i-1]['COUNT'] = len(temp1)
+
+tot = tohist.COUNT.sum()
+tohist.plot(kind = 'bar', color = 'steelblue', ax = ax3, legend = False)
+tohist['percent'] = (tohist['COUNT']/tot)*100
+tohist['percent'] = tohist['percent'].astype(float)
+tohist['percent'] = round(tohist['percent'], 1)
+for i in range(tohist.shape[0]):
+	plot.text(i, tohist.iloc[i]['COUNT'], str(tohist.iloc[i]['percent']), horizontalalignment = 'center')
+
+ax3.set_title('OCCASIONAL USERS')
+
+temp = df.loc[(df.FREQUENCY >= 15) & (df.FREQUENCY <= 50)]
+bin1 = list(range(0, 110, 10))
+fordf = []
+print(len(bin1))
+for i in range(len(bin1)):
+	if i == 0: continue
+	elif i == len(bin1): fordf.append("[{}, {}]".format(bin1[i-1], bin1[i]))
+	else: fordf.append("[{}, {})".format(bin1[i-1], bin1[i]))
+
+tohist = pd.DataFrame(index = fordf, columns = ['COUNT'])
+for i in range(len(bin1)):
+	print(bin1[i])
+	if i == 0: continue
+	elif i == len(bin1): temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION <= bin1[i])]
+	else: temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION < bin1[i])]
+
+	tohist.iloc[i-1]['COUNT'] = len(temp1)
+
+tot = tohist.COUNT.sum()
+tohist.plot(kind = 'bar', color = 'steelblue', ax = ax4, legend = False)
+tohist['percent'] = (tohist['COUNT']/tot)*100
+tohist['percent'] = tohist['percent'].astype(float)
+tohist['percent'] = round(tohist['percent'], 1)
+for i in range(tohist.shape[0]):
+	plot.text(i, tohist.iloc[i]['COUNT'], str(tohist.iloc[i]['percent']), horizontalalignment = 'center')
+
+ax4.set_title('DAILY USERS')
+
+temp = df.loc[(df.FREQUENCY >= 51)]
+bin1 = list(range(0, 110, 10))
+fordf = []
+print(len(bin1))
+for i in range(len(bin1)):
+	if i == 0: continue
+	elif i == len(bin1): fordf.append("[{}, {}]".format(bin1[i-1], bin1[i]))
+	else: fordf.append("[{}, {})".format(bin1[i-1], bin1[i]))
+
+tohist = pd.DataFrame(index = fordf, columns = ['COUNT'])
+for i in range(len(bin1)):
+	print(bin1[i])
+	if i == 0: continue
+	elif i == len(bin1): temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION <= bin1[i])]
+	else: temp1 = temp.loc[(temp.COMPLETION >= bin1[i-1]) & (temp.COMPLETION < bin1[i])]
+
+	tohist.iloc[i-1]['COUNT'] = len(temp1)
+
+tot = tohist.COUNT.sum()
+tohist.plot(kind = 'bar', color = 'steelblue', ax = ax5, legend = False)
+tohist['percent'] = (tohist['COUNT']/tot)*100
+tohist['percent'] = tohist['percent'].astype(float)
+tohist['percent'] = round(tohist['percent'], 1)
+for i in range(tohist.shape[0]):
+	plot.text(i, tohist.iloc[i]['COUNT'], str(tohist.iloc[i]['percent']), horizontalalignment = 'center')
+
+ax5.set_title('BINGE USERS')
 plt.tight_layout()
-plt.gcf().set_size_inches(5,5)
 plt.savefig('figures/bin5_distribution.png')
